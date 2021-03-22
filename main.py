5329ffefac2343e639648dfc8d04c163d056f7f4
vince <vincewcode@gmail.com>
2021-03-23 03:05:18 +0800

    added forms

--- main.py
+++ main.py
@@ -1,7 +1,52 @@
 from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 # flask instance
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "secret key"
+
+# Form Class
+class NameForm(FlaskForm):
+    name = StringField("What is your name", validators=[DataRequired()])
+    submit = SubmitField("Submit")
+
+    ## Fields
+	# BooleanField
+	# DateField
+	# DateTimeField
+	# DecimalField
+	# FileField
+	# HiddenField
+	# MultipleField
+	# FieldList
+	# FloatField
+	# FormField
+	# IntegerField
+	# PasswordField
+	# RadioField
+	# SelectField
+	# SelectMultipleField
+	# SubmitField
+	# StringField
+	# TextAreaField
+
+	## Validators
+	# DataRequired
+	# Email
+	# EqualTo
+	# InputRequired
+	# IPAddress
+	# Length
+	# MacAddress
+	# NumberRange
+	# Optional
+	# Regexp
+	# URL
+	# UUID
+	# AnyOf
+	# NoneOf
 
 # route decorator
 @app.route("/")
@@ -35,3 +80,14 @@ def notFound(e):
 @app.errorhandler(500)
 def serverDed(e):
     return render_template("500.html"), 500
+
+# Name page
+@app.route("/name", methods=["GET", "POST"])
+def name():
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ""
+    return render_template("name.html", name=name, form=form)
+
